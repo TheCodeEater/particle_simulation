2391590c2931024cf491b2f41fe3560cd5ec4204
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-10-10 12:43:12 +0200

    Added target general configuration and setup folder to create specific files

---
+++ project-config/Targets.cmake
@@ -0,0 +1,7 @@
+# Target configuration file
+# Targets should be defined in their own cmake file inside targets
+# Alternatively, they can be set up here, along with extra logic
+
+#List configuration files for targets to be compiled
+set(PROGRAM_TARGET_CONFIG
+        )
