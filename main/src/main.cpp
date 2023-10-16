f1d0771a0da8e2f5a2db83f27c4389e2e21f6087
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-10-16 16:43:34 +0200

    Removed useless includes

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,12 +1,8 @@
 //
 // Created by Giacomo Errani on 10/10/23.
 //
-#include "TCanvas.h"
-#include "TH1F.h"
-#include "TRandom.h"
 #include "TApplication.h"
 #include "TRint.h"
-#include "TRootCanvas.h"
 
 #include "Definitions.hpp"
 
