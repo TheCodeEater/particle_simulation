505215866d52ded5e9814bdaaf3eb7f2b5f30cf0
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-10-16 09:43:34 +0200

    Created basic example of root macro

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -2,17 +2,42 @@
 // Created by Giacomo Errani on 10/10/23.
 //
 #include "TCanvas.h"
-#include "TF1.h"
+#include "TH1F.h"
+#include "TRandom.h"
 #include "TApplication.h"
 #include "TRootCanvas.h"
 
-int main(int argc, char** argv){
+#include <random>
+
+int main(int argc, char** argv) {
     TApplication app("app", &argc, argv);
-    TCanvas* c = new TCanvas("c", "Something", 0, 0, 800, 600);
-    TF1 *f1 = new TF1("f1","sin(x)", -5, 5);
-    f1->SetLineColor(kBlue+1);
-    f1->SetTitle("My graph;x; sin(x)");
-    f1->Draw();
+
+    gRandom->SetSeed(std::random_device{}()); //generate random seed using hardware
+
+    TCanvas *c = new TCanvas("c", "Something", 0, 0, 800, 600);
+        TH1F *h =new TH1F("h","abundancies",5,0,5);
+//cosmetics
+        h->SetLineColor(1);
+        h->GetYaxis()->SetTitleOffset(1.2);
+        h->GetXaxis()->SetTitleSize(0.04);
+        h->GetYaxis()->SetTitleSize(0.04);
+        h->GetXaxis()->SetTitle("Cases");
+        h->GetYaxis()->SetTitle("Entries");
+        h->SetFillColor(4);
+        Double_t x=0;
+
+        for(Int_t i=0;i<1e5;i++){
+            x=gRandom->Rndm();
+            if(x<0.1)h->Fill(0);
+            else if(x<0.3)h->Fill(1);
+            else if(x<0.7)h->Fill(2);
+            else if(x<0.95)h->Fill(3);
+            else h->Fill(4);
+        }
+        h->SetMinimum(0);
+        h->SetFillColor(kGreen);
+        h->Draw("HIST");
+        h->Draw("PE*,SAME");
     c->Modified(); c->Update();
     TRootCanvas *rc = (TRootCanvas *)c->GetCanvasImp();
     rc->Connect("CloseWindow()", "TApplication", gApplication, "Terminate()");
