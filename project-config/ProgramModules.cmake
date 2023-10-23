29d5e825d9997790921011e68b3a96d2ae53b72f
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-10-23 21:46:19 +0200

    Commented sample pragma in linkdef, removde dummy header file from dictionary generation, splitting module definitions into sources and headers

--- project-config/ProgramModules.cmake
+++ project-config/ProgramModules.cmake
@@ -1,6 +1,7 @@
 # Program modules configuration
 # Defines variables that hold source/header files to be compilated
-# Variables must have this structure: PROGRAM_MODULES_<module_name>
+# Variables must have this structure: PROGRAM_MODULES_<module_name>_<SRC/H>
+# Main is the only exception
 # Do not recall data from libraries.cmake, unless you edit the CMakelists.txt appropriately
 # Note: use this only for program-specific code, if you plan to use shared components,
 # add them as a library
@@ -8,3 +9,7 @@ include_guard(DIRECTORY)
  set(PROGRAM_MODULES_MAIN
          ${PROGRAM_SOURCES}/main.cpp
          ${PROGRAM_HEADERS}/Definitions.hpp)
+
+#Example
+# set(PROGRAM_MODULES_GRAPHICS_SRC ${PROGRAM_SOURCES}/graphics.cpp)
+# set(PROGRAM_MDOULES_GRAPHICS_H ${PROGRAM_HEADERS}/graphics.hpp)
