de8446363a0be85b69f0241257a74ea406385317
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-10-23 21:37:50 +0200

    Added root dictionary generation

---
+++ main/dictionary/Linkdef.h
@@ -0,0 +1,14 @@
+//
+// Created by Giacomo Errani on 23/10/23.
+//
+
+#ifndef ROOT_TEMPLATE_PROJECT_LINKDEF_H
+#define ROOT_TEMPLATE_PROJECT_LINKDEF_H
+
+#ifdef __CLING__
+#pragma link C++ nestedclasses; //link nested classes
+#pragma link C++ nestedtypedefs; //link nested typedefs
+#pragma link C++ class MyClass;
+#endif
+
+#endif //ROOT_TEMPLATE_PROJECT_LINKDEF_H
