1775838be14d6d9b3bde735cc38ff62740d7c235
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-10-26 17:16:42 +0200

    Added particle adding in main

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -7,11 +7,22 @@
 
 #include "Definitions.hpp"
 
+#include "particles/Particle.hpp"
+
 int main(int argc, char** argv) {
     //object ownership setup
     #ifdef PROGRAM_USE_LOCAL_OWNERSHIP
         TDirectory::AddDirectory(kFALSE);
     #endif
+        BASE_NS::Particle::AddParticleType("PI+",0.13957, +1);
+        BASE_NS::Particle::AddParticleType("PI-",0.13957, -1);
+        BASE_NS::Particle::AddParticleType("K+",0.49367, +1);
+        BASE_NS::Particle::AddParticleType("K-",0.49367, -1);
+        BASE_NS::Particle::AddParticleType("P+",0.93827, +1);
+        BASE_NS::Particle::AddParticleType("P-",0.93827, -1);
+        BASE_NS::Particle::AddParticleType("K*",0.89166, 0,0.050); //resonance width only in unstable partivcle
+
+
     //create root application
     APP_TYPE app(APP_NAME, &argc, argv);
 
