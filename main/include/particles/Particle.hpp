3195840103154a8730b98e57606857284ddddedd
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-10-26 15:13:20 +0200

    Added add aprtciel type function

--- main/include/particles/Particle.hpp
+++ main/include/particles/Particle.hpp
@@ -7,6 +7,7 @@
 
 #include "Definitions.hpp"
 #include "particles/ParticleType.hpp"
+#include "particles/ResonanceType.hpp"
 
 #include <map>
 #include <string>
@@ -15,6 +16,13 @@ namespace BASE_NS{
     class Particle{
     public:
         explicit Particle(std::string const& name,double Px=0, double Py=0, double Pz=0);
+
+        [[nodiscard]] const std::string &GetParticleName() const;
+
+        static void AddParticleType(std::string const& name, double mass, int charge, double width=0);
+
+        void SetParticleType(std::string const& name);
+
     private:
         static std::map<std::string, std::unique_ptr<ParticleType>> fParticleType;
         static constexpr int fMaxNumParticleType{10};
