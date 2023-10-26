7f1b9db05f9a401efbf34fe45f892aed7a35142c
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-10-26 17:36:27 +0200

    Written preliminary particle generation code

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -4,10 +4,12 @@
 #include "TApplication.h"
 #include "TRint.h"
 #include "TDirectory.h"
+#include "TRandom.h"
 
 #include "Definitions.hpp"
-
 #include "particles/Particle.hpp"
+
+#include <algorithm>
 
 int main(int argc, char** argv) {
     //object ownership setup
@@ -22,6 +24,35 @@ int main(int argc, char** argv) {
         BASE_NS::Particle::AddParticleType("P-",0.93827, -1);
         BASE_NS::Particle::AddParticleType("K*",0.89166, 0,0.050); //resonance width only in unstable partivcle
 
+        //event generation
+        std::vector<BASE_NS::Particle> particles{};
+        particles.reserve(120);
+        std::generate_n(std::back_inserter(particles),100,[](){
+            //generate particle motion params
+            const double phi{gRandom->Uniform(0,2*M_PI)};
+            const double theta{gRandom->Uniform(0,M_PI)};
+            const double P{gRandom->Exp(1)};
+
+            //pick particle type
+            std::string particle_name{};
+            const double type{gRandom->Rndm()};
+
+            if(type>=0 && type<=0.4){
+                particle_name="PI+";
+            }else if(type<0.8){
+                particle_name="PI-";
+            }else if(type<0.85){
+                particle_name="K+";
+            }else if(type<0.9){
+                particle_name="K-";
+            }else if(type<0.945){
+                particle_name="P+";
+            }else if(type<0.99){
+                particle_name="P-";
+            }else{
+                particle_name="K*";
+            }
+        });
 
     //create root application
     APP_TYPE app(APP_NAME, &argc, argv);
