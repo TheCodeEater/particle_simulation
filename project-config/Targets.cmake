6c3aaa3447b470604890c93fd5a5da115691921a
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-10-26 15:50:05 +0200

    Added modules

--- project-config/Targets.cmake
+++ project-config/Targets.cmake
@@ -9,4 +9,5 @@ add_subdirectory(${CMAKE_SOURCE_DIR}/main)
 #Add tests
 if(BUILD_TESTING)
     add_subdirectory(${PROGRAM_TEST_UNIT}/sample) #add example test
+    add_subdirectory(${PROGRAM_TEST_UNIT}/particleTest)
 endif()
