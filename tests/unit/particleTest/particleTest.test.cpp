2bdd75ff45140fc91949152e2dfe48fbe071ce0e
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-10-26 15:48:17 +0200

    Added tests

---
+++ tests/unit/particleTest/particleTest.test.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Giacomo Errani on 26/10/23.
+//
+#include "particles/ParticleType.hpp"
+#include "particles/ResonanceType.hpp"
+#include "Definitions.hpp"
+
+using namespace BASE_NS;
+
+int main(){
+    ParticleType part{"part1", 1, 16};
+    ResonanceType pawrt{"pawrt1", 2, 32, 8.};
+
+    part.Print();
+    pawrt.Print();
+}
