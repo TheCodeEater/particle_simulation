533b10af395be8837e6d2fdb09e8003209387794
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-11-02 22:00:21 +0100

    Created ctor for class, written getter for file

--- analyser/include/particleAnalyser.hpp
+++ analyser/include/particleAnalyser.hpp
@@ -6,9 +6,33 @@
 #define ROOT_TEMPLATE_PROJECT_PARTICLEAANALYSER_HPP
 
 #include "Definitions.hpp"
+#include "TFile.h"
+#include <string>
 
 namespace BASE_NS{
+    /**
+     * This class encapsulates the data analysis for the K* decayment trace
+     */
+    class dataAnalyser{
 
+    public:
+        /**
+         * Construct the analyser class
+         * @param path Path to the ROOT file holding the required objects
+         */
+        explicit dataAnalyser(std::string const& path);
+
+        /**
+         *
+         */
+         [[nodiscard]] TFile const& getFile() const;
+
+    private:
+        /**
+         * A pointer to an instance of a root file
+         */
+        std::unique_ptr<TFile> fFile;
+    };
 }
 
 #endif //ROOT_TEMPLATE_PROJECT_PARTICLEAANALYSER_HPP
--- analyser/src/particleAnalyser.cpp
+++ analyser/src/particleAnalyser.cpp
@@ -6,4 +6,21 @@
 
 namespace BASE_NS{
 
+    dataAnalyser::dataAnalyser(const std::string &path):
+        //open the root file in read only mode
+        fFile{new TFile{path.c_str(),"READ"}}{
+
+        //check that the file was opened correctly
+        if(fFile->IsZombie()){
+            throw std::runtime_error{"Cannot open root file. Path: "+path};
+        }
+    }
+
+    TFile const &dataAnalyser::getFile() const {
+        //check wether file is valid using unique_ptr bool conversion operator
+        if(!fFile){
+            throw std::runtime_error{"Root file object is invalid!"};
+        }
+        return *fFile;
+    }
 }
