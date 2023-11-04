362413d1202166768119535aff31b902ccd8ac9a
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-11-04 11:03:31 +0100

    Written storage structure for histograms

--- analyser/include/particleStorage.hpp
+++ analyser/include/particleStorage.hpp
@@ -11,57 +11,25 @@
 
 namespace BASE_NS{
     /**
-     * This class is used to store particle data
+     * Store datas to be analysed
+     * The storage type may vary across different versions
      */
-    class particleStorage: public TObject{
-        using key=std::string;
-        public:
-            //store in unordered map th1 float histograms
-            using HistContainer=std::unordered_map<key,TH1F>;
-            /**
-             * Construct empty object
-             */
-            particleStorage()=default;
-            /**
-             * Construct the storage object from an already existing container
-             * Note: the container is internally moved
-             * @param cont The container holding the histogram requested
-             */
-            explicit particleStorage(HistContainer const& cont): fHistContainer{std::move(cont)}{}
-            /**
-             * Register a new histogram into the container
-             * @tparam histArgs Arguments to be forwarded to the TH1F constructor
-             * @param k Key used to identify the histogram
-             * @param args Arguments used to construct the histogram
-             */
-            template<typename ...histArgs>
-            void addHistogram(key const& k,histArgs&& ...args){
-                //check that if does exist
-                if(fHistContainer.find(k)!=fHistContainer.end()){
-                    throw std::runtime_error{"Key already exists! Key: "+k};
-                }
-                //forward hist construction arguments to hist ctor
-                //pass key first to be the 1st member of pair (underlying node)
-                fHistContainer.emplace(k,std::forward<histArgs>(args)...);
-            }
-            /**
-             * Get the underlying container - const version
-             * @return Const reference to the underlying container
-             */
-            [[nodiscard]] HistContainer const& GetContainer() const{
-                return fHistContainer;
-            }
-        /**
-         * Get the underlying container - non const version
-         * @return Reference to the underlying container
-         */
-            HistContainer& GetContainer(){
-                return fHistContainer;
-            }
+    struct particleStorage: public TObject{
+        using histCont=std::unique_ptr<std::string,TH1F>;
+        using hist=TH1F;
+
+        //Histograms classified by type
+        hist GeneratedTypes;
+        hist PolarAngles;
+        hist AzimuthAngles;
+        hist Pulse;
+        hist TransPulse;
+        hist Energy;
+
+        //store all invariant masses histograms into a container
+        histCont invMasses;
 
         ClassDefOverride(particleStorage,1);
-    private:
-        HistContainer fHistContainer; ///Container holding histograms
     };
 }
 
--- analyser/src/particleStorage.cpp
+++ analyser/src/particleStorage.cpp
@@ -1,3 +1,11 @@
 //
 // Created by Giacomo Errani on 04/11/23.
 //
+#include "particleStorage.hpp"
+
+#include "Rtypes.h"
+
+namespace BASE_NS{
+    //to figure out why it does not work
+    //ClassImp(particleStorage);
+}
