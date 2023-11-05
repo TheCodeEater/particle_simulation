7f0d0d72a3857df32b0fee9552af2b2815b2433a
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-11-05 15:20:48 +0100

    Rewriting graphgic class. Storing fit data

--- analyser/include/analyserGraphics.hpp
+++ analyser/include/analyserGraphics.hpp
@@ -14,39 +14,18 @@
 
 namespace BASE_NS{
     /**
-     * This enums defines the possible scenes that can be displayed
-     */
-    enum class Menu{
-        mainMenu, /// Choice menu with buttons (lileky won't be implemented)
-        generationHistograms, /// Generated data
-        kinematicHistograms, /// Pulse and energy
-        invMassHistograms, /// InvariantMass
-        signalHistograms /// Decayment signals
-    };
-    /**
      * This class encapsulates graphics for the particle data analysis
      */
     class AnalyzerGraphics{
         using CanvasPtr=std::unique_ptr<TCanvas>;
-        using screenDataStorage=std::unordered_map<Menu,std::unique_ptr<TList>>;
 
     public:
         explicit AnalyzerGraphics(dataAnalyser& analyser);
 
-        /**
-         * Change currently active screen
-         * @param screen the screen to be shown
-         */
-        void ShowScreen(Menu screen);
-
     private:
-        //std::shared_ptr<dataAnalyser> fDataAnalyser; /// Pointer to the object handling analysed data
-        //std::shared_ptr<SignalResult> fSignalResult;
-
-        Menu fActiveScreen; /// Currently active screen
+        std::shared_ptr<particleStorage> fInputData; /// Pointer to the object handling analysed data
+        std::shared_ptr<SignalResult> fSignalResult;
         CanvasPtr fCanvas; /// Canvas used to show data
-
-        screenDataStorage fGraphicData; /// store graphical objects for each screen
     };
 }
 
--- analyser/src/analyserGraphics.cpp
+++ analyser/src/analyserGraphics.cpp
@@ -5,52 +5,17 @@
 
 #include <algorithm>
 
-BASE_NS::AnalyzerGraphics::AnalyzerGraphics(dataAnalyser& analyser):
-    //fDataAnalyser{std::move(analyser)}, //shared pointer already copied. Just move
-    //fSignalResult{fDataAnalyser->GetDecaymentSignal()}, //run fit
-    fActiveScreen{Menu::mainMenu}, //set main menu
-    fCanvas{new TCanvas()}//create empty canvas
-        {
-            //Get input data
-            auto inputData=analyser.GetData();
-            //populate menu lists
-            //populate geneated data menu
-            {
-                auto genList=std::make_unique<TList>();
-                genList->Add(& inputData->GeneratedTypes);
-                genList->Add(& inputData->PolarAngles);
-                genList->Add(& inputData->AzimuthAngles);
+namespace BASE_NS{
+    AnalyzerGraphics::AnalyzerGraphics(dataAnalyser& analyser):
+            fInputData{analyser.GetData()}, //get input data
+            fSignalResult{analyser.GetDecaymentSignal()}, //run fit
+            fCanvas{new TCanvas()}//create empty canvas
+    {
 
-                fGraphicData[Menu::generationHistograms]=std::move(genList);
-            }
-            //populate kinematic histograms
-            {
-                auto kinList=std::make_unique<TList>();
-                kinList->Add(&inputData->Pulse);
-                kinList->Add(&inputData->Energy);
-                kinList->Add(&inputData->TransPulse);
 
-                fGraphicData[Menu::kinematicHistograms]=std::move(kinList);
-            }
-            //populate invMass histograms
-            {
-                auto invList=std::make_unique<TList>();
-
-                //for each histogram about invariant mass
-                for(auto& node: inputData->invMasses){
-                    invList->Add(std::addressof(node.second)); //get ptr to underlying data
-                }
-
-                fGraphicData[Menu::invMassHistograms]=std::move(invList);
-            }
-            //populate signal histograms
-            {
-
-            }
-
-        }
-
-void BASE_NS::AnalyzerGraphics::ShowScreen(BASE_NS::Menu screen) {
-    fActiveScreen=screen; //set the currenlty active screen
+    }
 }
+
+
+
 
