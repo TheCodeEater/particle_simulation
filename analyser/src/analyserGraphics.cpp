759a4e309cdbaf4040f0fa3cf1f715ff6c9c5827
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-11-05 15:32:26 +0100

    Improved canvas

--- analyser/src/analyserGraphics.cpp
+++ analyser/src/analyserGraphics.cpp
@@ -9,7 +9,7 @@ namespace BASE_NS{
     AnalyzerGraphics::AnalyzerGraphics(dataAnalyser& analyser):
             fInputData{analyser.GetData()}, //get input data
             fSignalResult{analyser.GetDecaymentSignal()}, //run fit
-            fCanvas{new TCanvas()},//create empty canvas
+            fCanvas{new TCanvas("mainCanvas","K* signal analyser",1200,800)},//create empty canvas
             fViews{}
     {
             fViews["generationHistograms"]=std::make_unique<TPad>(
