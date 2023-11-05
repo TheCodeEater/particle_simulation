fdc02b27a6839e193a0c0f45b1728f92c365b18a
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-11-05 10:51:05 +0100

    Written basic graphic class

--- analyser/include/analyserGraphics.hpp
+++ analyser/include/analyserGraphics.hpp
@@ -5,4 +5,33 @@
 #ifndef ROOT_TEMPLATE_PROJECT_ANALYSERGRAPHICS_HPP
 #define ROOT_TEMPLATE_PROJECT_ANALYSERGRAPHICS_HPP
 
+#include <memory>
+#include "TCanvas.h"
+
+namespace BASE_NS{
+    /**
+     * This enums defines the possible scenes that can be displayed
+     */
+    enum class Menu{
+        mainMenu, /// Choice menu with buttons (lileky won't be implemented)
+        generationHistograms, /// Generated data
+        kinematicHistograms, /// Pulse and energy
+        invMassHistograms, /// InvariantMass
+        signalHistograms /// Decayment signals
+    };
+    /**
+     * This class encapsulates graphics for the particle data analysis
+     */
+    class AnalyzerGraphics{
+        using CanvasPtr=std::unique_ptr<TCanvas>;
+
+    public:
+        AnalyzerGraphics();
+
+    private:
+        Menu fActiveScreen; /// Currently active screen
+        CanvasPtr fCanvas; /// Canvas used to show data
+    };
+}
+
 #endif //ROOT_TEMPLATE_PROJECT_ANALYSERGRAPHICS_HPP
--- analyser/src/analyserGraphics.cpp
+++ analyser/src/analyserGraphics.cpp
@@ -1,3 +1,11 @@
 //
 // Created by Giacomo Errani on 05/11/23.
 //
+#include "analyserGraphics.hpp"
+
+BASE_NS::AnalyzerGraphics::AnalyzerGraphics():
+    fActiveScreen{Menu::mainMenu},
+    fCanvas{new TCanvas()} {
+
+}
+
