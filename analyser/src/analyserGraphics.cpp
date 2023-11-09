ef0edb825719f3df04044f3f38139caf316d477c
Stella Baldrati <sbaldrati3@gmail.com>
2023-11-09 15:45:26 +0100

    Wrote most of the canvas, except the one with the sums oh, and i shout out in the classroom because i had headphones and coudn't hear lol

--- analyser/src/analyserGraphics.cpp
+++ analyser/src/analyserGraphics.cpp
@@ -7,11 +7,32 @@
 
 namespace BASE_NS{
     AnalyzerGraphics::AnalyzerGraphics(dataAnalyser& analyser):
-            fInputData{analyser.GetData()}, //get input data
-            fSignalResult{analyser.GetDecaymentSignal()}, //run fit
-            fCanvas{new TCanvas("mainCanvas","K* signal analyser",1200,800)},//create empty canvas
-    {
-            
+        fInputData{analyser.GetData()}, //get input data
+        fSignalResult{analyser.GetDecaymentSignal()}, //run fit
+
+        MassCanvas{new TCanvas("massCanvas",1200,800)},
+        PartTypeCanvas{new TCanvas("PartTypeCanvas",1200,800)},
+        DistributionsCanvas{new TCanvas("DistributionsCanvas",1200,800)},
+        SumCanvas{new TCanvas("SumCanvas",1200,800)}{
+                MassCanvas->Divide(3,2);
+                        for(int i=0; i<fInputData->invMassess.size(); i++){
+                        MassCanvas->cd(i);
+                        fInputData->invMasses[i].Draw();
+                         }
+                PartTypeCanvas->cd(0);
+                        GeneratedTypes->Draw();
+                DistributionsCanvas->Divide(2,1);
+                        DistributionsCanvas->cd(0);
+                                PolarAngles->Draw();
+                        DistributionsCanvas->cd(1);
+                                AzimuthAngles->cd();
+                SumCanvas->Divide(2,1);
+                        SumCanvas->cd(0);
+                                
+                        SumCanvas->cd(1);
+
+
+    
     }
 }
 
