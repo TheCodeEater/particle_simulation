b6698881fabed495686115a8fdd06c8fadedd0d7
Stella Baldrati <sbaldrati3@gmail.com>
2023-11-09 15:11:34 +0100

    Created canvas for histogram, similar with similar

--- analyser/src/analyserGraphics.cpp
+++ analyser/src/analyserGraphics.cpp
@@ -10,27 +10,8 @@ namespace BASE_NS{
             fInputData{analyser.GetData()}, //get input data
             fSignalResult{analyser.GetDecaymentSignal()}, //run fit
             fCanvas{new TCanvas("mainCanvas","K* signal analyser",1200,800)},//create empty canvas
-            fViews{}
     {
-            fViews["generationHistograms"]=std::make_unique<TPad>(
-                "genHist",
-                "Histograms of generation distribution",
-                0,0,1,1);
-            fViews["kinematicHistograms"]=std::make_unique<TPad>(
-                    "kineticHist",
-                    "Histograms of particle kinetic data",
-                    0,0,1,1
-                    );
-            fViews["invariantMassesHistograms"]=std::make_unique<TPad>(
-                    "invMassHist",
-                    "Histograms of invariant masses",
-                    0,0,1,1
-                    );
-            fViews["signalHistograms"]=std::make_unique<TPad>(
-                    "sigHist",
-                    "Histograms of K* signals",
-                    0,0,1,1
-                    );
+            
     }
 }
 
