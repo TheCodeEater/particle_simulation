d2782fbe7f41fab396eec9f33f211c78b7cc5156
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-11-21 11:47:22 +0100

    Adding update calling

--- analyser/src/analyserGraphics.cpp
+++ analyser/src/analyserGraphics.cpp
@@ -16,6 +16,8 @@ namespace BASE_NS{
         DistributionsCanvas{new TCanvas("DistributionsCanvas","DistributionsCanvas",1200,800)},
         SumCanvas{new TCanvas("SumCanvas","SumCanvas",1200,800)},
         PulseCanvas{new TCanvas("PulseCanvas","PulseCanvas",1200,800)}{
+                const char* options{"HIST"};
+
                 MassCanvas->Divide(3,2);
                         /*int i{};
                         for(auto & node: fInputData->invMasses){
@@ -26,34 +28,45 @@ namespace BASE_NS{
                          }*/
                         //MassCanvas->cd(0);
                         MassCanvas->cd(0);
-                        fInputData->InvariantMassesDprod.Draw();
+                        fInputData->InvariantMassesDprod.Draw(options);
 
 
                 PartTypeCanvas->cd(0);
-                        fInputData->ParticlesType.Draw();
+                        fInputData->ParticlesType.Draw(options);
                 DistributionsCanvas->Divide(2,1);
                         DistributionsCanvas->cd(0);
-                            fInputData->PolarAngles.Draw();
+                            fInputData->PolarAngles.Draw(options);
                         DistributionsCanvas->cd(1);
-                            fInputData->AzimuthalAngles.Draw();
+                            fInputData->AzimuthalAngles.Draw(options);
                 SumCanvas->Divide(2,1);
                         SumCanvas->cd(0);
-                                fSignalResult->signal1.Draw();
+                                fSignalResult->signal1.Draw(options);
                         SumCanvas->cd(1);
-                                fSignalResult->signal2.Draw();
+                                fSignalResult->signal2.Draw(options);
                 PulseCanvas->Divide(3,1);
                         PulseCanvas->cd(0);
-                            fInputData->Impulse.Draw();
+                            fInputData->Impulse.Draw(options);
                         PulseCanvas->cd(1);
-                            fInputData->TransverseImpulse.Draw();
+                            fInputData->TransverseImpulse.Draw(options);
                         PulseCanvas->cd(2);
-                             fInputData->Energies.Draw();
+                             fInputData->Energies.Draw(options);
 
+                             MassCanvas->Modified();
+        PartTypeCanvas->Modified();
+        DistributionsCanvas->Modified();
+        SumCanvas->Modified();
+        PulseCanvas->Modified();
 
+                            MassCanvas->Update();
                              PartTypeCanvas->Update();
                              DistributionsCanvas->Update();
                              SumCanvas->Update();
                              PulseCanvas->Update();
+
+        PartTypeCanvas->Print("particleTypes.gif");
+        DistributionsCanvas->Print("generationDistributions.gif");
+        SumCanvas->Print("sumCanvas.gif");
+        PulseCanvas->Print("pulseCanvas.gif");
 
     
     }
