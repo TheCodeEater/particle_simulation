082c5aebee3764cddfbf539e63919680dd8463a1
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-12-30 17:11:13 +0100

    Cleaning main

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,41 +1,37 @@
 //
 // Created by Giacomo Errani on 10/10/23.
 //
-#include <deque>
-#include <iostream>
-
-#include "../include/particles/Particle.hpp"
 #include "Definitions.hpp"
+
 #include "TApplication.h"
 #include "TDirectory.h"
+#include <TH1.h>
 #include "TFile.h"
-#include "TH1.h"
-#include "TMath.h"
-#include "TRandom.h"
-#include "TRint.h"
+
 #include "generator/ParticleGenerator.hpp"
-#include "generator/ProportionGenerator.hpp"
 #include "particleStorage.hpp"
 #include "particles/ParticleType.hpp"
 
 int main(int argc, char **argv) {
     // object ownership setup
-    // #ifdef PROGRAM_USE_LOCAL_OWNERSHIP
-    //    TDirectory::AddDirectory(kFALSE);
-    // #endif
-    // create root application
-    // APP_TYPE app(APP_NAME, &argc, argv);
+    #ifdef PROGRAM_USE_LOCAL_OWNERSHIP
+        TDirectory::AddDirectory(kFALSE);
+        TH1::AddDirectory(kFALSE);
+    #endif
 
+    //allocate memory for particle data
     ResonanceSimulator::particleGenerator::loadParticles();
 
+    //create generator object
     ResonanceSimulator::particleGenerator generator{};
     // run the simulation
     // save result in unique pointer
     std::unique_ptr<ResonanceSimulator::particleStorage> result{generator(1e5)};
-    auto *file = new TFile("Particle.root", "RECREATE");
+
+    //write to file
+    std::unique_ptr<TFile> output_file{new TFile("Particle.root", "RECREATE")};
     result->Write();
-    file->Close();
-    // run application
-    // app.Run();
+    output_file->Close();
+
     return 0;
 }
